811bbab66f0d902954429a2b61821b197449627c
Sreehari Sreedev <me@ssree.dev>
2024-02-28 03:08:50 -0700

    nix: Replace jakt.nix with flake
    This allows for reproducible builds and insane ease of use.
    Now, anyone with a Linux machine with nix installed
    can just run `nix build` and easily build or install jakt.
    
    Further, jakt contributors can easily get a shell with all
    the dependencies just by running `nix develop`.

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "The Jakt Programming Language";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    gitignore = {
+      url = github:hercules-ci/gitignore.nix;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gitignore }: let
+    forAllSystems = nixpkgs.lib.genAttrs flake-utils.lib.defaultSystems;
+    inherit (gitignore.lib) gitignoreSource;
+  in {
+    overlays.default = (final: prev:
+      let
+        buildInputs = with prev; [
+          clang
+          python3
+        ];
+        nativeBuildInputs = with prev; [
+          pkg-config
+          cmake
+          ninja
+       ];
+      in {
+        jakt = final.callPackage ./jakt.nix {
+          inherit gitignoreSource;
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
+    );
+
+    packages = forAllSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+        jakt = pkgs.jakt;
+        default = jakt;
+      in rec {
+        inherit default jakt;
+      }
+    );
+    devShells = forAllSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          inherit (self.packages.${system}.default) buildInputs nativeBuildInputs;
+        };
+      }
+    );
+  };
+}
--- jakt.nix
+++ jakt.nix
@@ -1,13 +1,26 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell
-{
-  name = "jakt";
+{ stdenv
+, lib
+, gitignoreSource
+, buildInputs
+, nativeBuildInputs
+, clang
+, fetchFromGitHub
+}:
 
-  nativeBuildInputs = with pkgs; [
-    pkgconfig
-    cmake
-    ninja
-    python3
-    clang_15
+stdenv.mkDerivation (finalAttrs: let
+  serenity = fetchFromGitHub {
+    owner = "serenityos";
+    repo = "serenity";
+    rev = "05e78dabdbceea46bae7dca52b63dc0a115e7b52"; # latest at the time
+    hash = "sha256-ymXQ68Uib1xP4eGPuxm3vRgAIhrVK4rmHdGLfuvsOJU=";
+  };
+in {
+  inherit buildInputs nativeBuildInputs;
+  name = "jakt";
+  src = gitignoreSource ./.;
+  cmakeFlags = [
+    "-DCMAKE_CXX_COMPILER=${clang}/bin/clang++"
+    "-DSERENITY_SOURCE_DIR=${serenity}"
+    "-DCMAKE_INSTALL_BINDIR=bin"
   ];
-}
+})
